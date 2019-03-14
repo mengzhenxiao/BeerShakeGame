a7390c671cc1e1fba1502240d9badf6ab126dbf6
mengzhenxiao <mx536@nyu.edu>
2019-03-14 18:19:01 -0400

    mengzhen's commit

---
+++ ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BeerShakeGame
+//
+//  Created by 肖梦真 on 3/14/19.
+//  Copyright © 2019 mengzhenxiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
