e943c74ffc7986b142a68ce9ae8e9357c96fa3d3
mengzhenxiao <mx536@nyu.edu>
2019-03-14 17:01:22 -0400

    Initial Commit

---
+++ BeerShakeGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerShakeGame
+//
+//  Created by 肖梦真 on 3/14/19.
+//  Copyright © 2019 mengzhenxiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
